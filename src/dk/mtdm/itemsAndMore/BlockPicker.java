5ccbf70dd74d06006d644c78de4e454906ca01f1
Lukas <lukasvranic@gmail.com>
2023-01-22 15:51:00 +0100

    cleaned switch

--- src/dk/mtdm/itemsAndMore/BlockPicker.java
+++ src/dk/mtdm/itemsAndMore/BlockPicker.java
@@ -1,64 +1,70 @@
 package dk.mtdm.itemsAndMore;
 
+import javax.swing.text.html.HTMLDocument.RunElement;
+
 import dk.mtdm.LDVector;
 import dk.mtdm.exceptions.MissingBlockTypeException;
 
 public class BlockPicker {
 	/**
-	* Creates a block from BlockTypes
-	* @param type the type of block
-	* @param x,y sets x,y-canvas location value of the block
-	*/
-	static public Block picker(BlockTypes type,int x,int y) throws MissingBlockTypeException {
+	 * Creates a block from BlockTypes
+	 * 
+	 * @param type the type of block
+	 * @param x,y  sets x,y-canvas location value of the block
+	 */
+	static public Block picker(BlockTypes type, int x, int y) throws MissingBlockTypeException {
 		return BlockPicker.picker(type, new LDVector(x, y));
 	}
 
 	/**
-	* Creates a block from BlockTypes
-	* @param pos Sets the canvas location of the block
-	* @param id id/type of block
-	*/
-	static public Block picker(BlockTypes type, LDVector pos) throws MissingBlockTypeException{
-		Block block = new Block(pos, type);		
+	 * Creates a block from BlockTypes
+	 * 
+	 * @param pos Sets the canvas location of the block
+	 * @param id  id/type of block
+	 */
+	static public Block picker(BlockTypes type, LDVector pos) throws MissingBlockTypeException {
+		Block block = new Block(pos, type);
+
 		switch (type) {
 			case air -> {
 				block.setSolidity(false);
 				block.setBreakability(false);
 				block.setHoverability(false);
 				block.setItemDrop(null);
-				return block;
+				break;
 			}
 			case grass -> {
 				block.setSolidity(true);
 				block.setBreakability(true);
 				block.setHoverability(true);
 				block.setItemDrop(ItemTypes.dirt);
-				return block;
+				break;
 			}
 			case dirt -> {
 				block.setSolidity(true);
 				block.setBreakability(true);
 				block.setHoverability(true);
 				block.setItemDrop(ItemTypes.dirt);
-				return block;
+				break;
 			}
 			case stone -> {
 				block.setSolidity(true);
 				block.setBreakability(true);
 				block.setHoverability(true);
 				block.setItemDrop(ItemTypes.stone);
-				return block;
+				break;
 			}
 			case bedrock -> {
 				block.setSolidity(true);
 				block.setBreakability(true);
 				block.setHoverability(true);
 				block.setItemDrop(null);
-				return block;
+				break;
 			}
 			default -> {
 				throw new MissingBlockTypeException("This is not a block type!!: " + type.toString());
 			}
 		}
+		return block;
 	}
 }
