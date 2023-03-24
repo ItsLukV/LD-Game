9235570e092035bde60ae7237c28673073f30069
dendersen <dendersen@endersen.dk>
2023-03-24 22:06:41 +0100

    list

--- src/dk/mtdm/managementSystem/world/ChunkList.java
+++ src/dk/mtdm/managementSystem/world/ChunkList.java
@@ -1,79 +1,114 @@
 package dk.mtdm.managementSystem.world;
 
+import java.util.LinkedList;
+
 public class ChunkList {
-  private static chunkContainer chunks;
+  private static LinkedList<chunkContainer> chunks;
+  private static int dimensionID; 
   public static void setup(Chunk firstChunk){
-    ChunkList.chunks = new chunkContainer(firstChunk);
+    dimensionID = DimensionTypes.Overworld.ordinal();
+    ChunkList.chunks = new LinkedList<chunkContainer>();
+    for (int i = 0; i < DimensionTypes.values().length; i++) {
+      chunks.add(null);
+    }
+    ChunkList.chunks.set(dimensionID,new chunkContainer(firstChunk,0));
   }
   public static Chunk getChunk(int ID){
-    return chunks.get(ID);
+    return chunks.get(dimensionID).get(ID);
   }
   public static void generate(int ID){
-    chunks.generate(ID);
+    chunks.get(dimensionID).generate(ID);
   }
   public static void extendRight(Chunk chunk){
-    chunks.extendAfter(chunk);
+    chunks.get(dimensionID).extendAfter(chunk);
   }
   public static void extendLeft(Chunk chunk){
-    chunks.extendBefore(chunk);
+    chunks.get(dimensionID).extendBefore(chunk);
   }
   public static void replace(int ID,Chunk chunk){
-    chunks.replace(ID, chunk);
+    chunks.get(dimensionID).replace(ID, chunk);
+  }
+  public static void setDimensionID(int ID){
+    if(ID < DimensionTypes.values().length){
+      setSpecifiedDimensions(DimensionTypes.values()[ID]);
+      return;
+    }
+    setUnspecifiedDimensions(ID);
+  }
+  public static void setDimensionID(DimensionTypes Dimension){
+    setSpecifiedDimensions(Dimension);
+  }
+  private static void setSpecifiedDimensions(DimensionTypes Dimension) {
+    //TODO
+  }
+  private static void setUnspecifiedDimensions(int Dimension) {
+    //TODO
+  }
+  public static void setChunkContainerStart(chunkContainer start){
+    chunks.set(dimensionID,start);
   }
 
   static class chunkContainer {
     private Chunk containedChunk;
     private chunkContainer after; //higher ID
     private chunkContainer before; //lower ID
+    private int ID; 
     
-    public chunkContainer(Chunk chunk){
+    public chunkContainer(Chunk chunk, int ID){
       containedChunk = chunk;
+      this.ID = ID;
     }
+    
     public void extendAfter(Chunk chunk){
       if(after != null){
         after.extendAfter(chunk);
         return;
       }
-      after = new chunkContainer(chunk);
+      after = new chunkContainer(chunk,this.ID+1);
+      after.setbefore(this);
     }
     public void extendBefore(Chunk chunk){
       if(before != null){
         before.extendAfter(chunk);
         return;
       }
-      before = new chunkContainer(chunk);
+      before = new chunkContainer(chunk,this.ID-1);
+      before.setAfter(this);
     }
     public Chunk get(int ID){
-      if(ID > 0){
+      if(ID > this.ID){
         if(after == null){
           return null;
         }
-        return after.get(ID-1);
-      } else if(ID < 0){
+        return after.get(ID);
+      } else if(ID < this.ID){
         if(before == null){
           return null;
         }
-        return before.get(ID+1);
+        return before.get(ID);
       }else{
+        ChunkList.setChunkContainerStart(this);
         return containedChunk;
       }
     }
     public void replace(int ID, Chunk chunk){
-      if(ID > 0){
+      if(ID > this.ID){
         if(after == null){
-          after = new chunkContainer(World.standardChunk(containedChunk.getID()+1));
+          after = new chunkContainer(World.standardChunk(containedChunk.getID()+1),ID+1);
           after.generator();
+          after.setbefore(this);
           return;
         }
-        after.replace(ID-1,chunk);
+        after.replace(ID,chunk);
         return;
-      } else if(ID < 0){
+      } else if(ID < this.ID){
         if(before == null){
-          before = new chunkContainer(World.standardChunk(containedChunk.getID()-1));
+          before = new chunkContainer(World.standardChunk(containedChunk.getID()-1),ID-1);
           before.generator();
+          before.setAfter(this);
           return;
         }
-        before.replace(ID+1, chunk);
+        before.replace(ID, chunk);
         return;
       }else{
         containedChunk = chunk;
@@ -81,21 +116,23 @@ public class ChunkList {
       }
     }
     public void generate(int ID){
-      if(ID > 0){
+      if(ID > this.ID){
         if(after == null){
-          after = new chunkContainer(World.standardChunk(containedChunk.getID()+1));
+          after = new chunkContainer(World.standardChunk(containedChunk.getID()+1),this.ID+1);
           after.generator();
+          after.setbefore(this);
           return;
         }
-        after.generate(ID-1);
+        after.generate(ID);
         return;
-      } else if(ID < 0){
+      } else if(ID < this.ID){
         if(before == null){
-          before = new chunkContainer(World.standardChunk(containedChunk.getID()-1));
+          before = new chunkContainer(World.standardChunk(containedChunk.getID()-1),this.ID-1);
           before.generator();
+          before.setAfter(this);
           return;
         }
-        before.generate(ID+1);
+        before.generate(ID);
         return;
       }else{
         generator();
@@ -104,6 +141,12 @@ public class ChunkList {
     }
     private void generator(){
       containedChunk.generate();
+    }
+    public void setbefore(chunkContainer chunk){
+      before = chunk;
+    }
+    public void setAfter(chunkContainer chunk){
+      after = chunk;
     }
   }
 }
---
+++ src/dk/mtdm/managementSystem/world/DimensionTypes.java
@@ -0,0 +1,7 @@
+package dk.mtdm.managementSystem.world;
+
+public enum DimensionTypes {//world will start in overworld, go up in ordinal to find high dimensions and down to find low dimensions
+  Nether,
+  Overworld,
+  end,
+}
